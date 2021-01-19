cb9e26aaa6b0191d9691a4f4fd8096fb9653c094
Yulya Oleshko <uliaolescko@gmail.com>
2021-01-19 11:36:26 +0200

    hw_18

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+let testPerformance = (iterations, func) => {
+    let time = Date.now();
+  
+    if (typeof func === 'function') for (let i = iterations; i--;) func();
+  
+    return Date.now() - time;
+  };
+  function test1() {
+    let str = myLongStr;
+    while (str.indexOf('o') !== -1) str = str.replace('o', '');
+    while (str.indexOf('a') !== -1) str = str.replace('a', '');
+    while (str.indexOf('e') !== -1) str = str.replace('e', '');
+    while (str.indexOf('u') !== -1) str = str.replace('u', '');
+    while (str.indexOf('i') !== -1) str = str.replace('i', '');
+  }
+  
+  // данная функция необходима для корректного тестирования кода
+  function test2() {
+    const reg = new RegExp('[oaeui]', 'gui');
+  
+    myLongStr.replace(reg, '');
+  }
+  
+  console.log(testPerformance(100, test1)); // time
+  console.log(testPerformance(100, test2)); // time
+  console.log(testPerformance(100, 12345)); // 0
